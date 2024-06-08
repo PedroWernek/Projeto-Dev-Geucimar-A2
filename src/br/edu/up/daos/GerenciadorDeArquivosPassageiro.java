61f58f148cf94862b479d32027580e6f503ad753
Ana Paula Endler <anapaulaendler@gmail.com>
2024-06-08 09:14:22 -0300

    pequenas alterações

--- src/br/edu/up/daos/GerenciadorDeArquivosPassageiro.java
+++ src/br/edu/up/daos/GerenciadorDeArquivosPassageiro.java
@@ -1,19 +1,20 @@
 package br.edu.up.daos;
 
+import java.io.File;
 import java.util.*;
 import br.edu.up.modelos.Passageiro;
 
 public class GerenciadorDeArquivosPassageiro {
     
     public String header = "";
-    private String nomeDoArquivo = "";
+    private String nomeDoArquivo = "C:/Users/Usuário/Documents/Projeto-Engenharia-de-Software---sistema-de-trafego-aereo/src/br/edu/up/docs";
 
     /* lê os arquivos */
     public List<Passageiro> getPassageiro() {
         List<Passageiro> listaDePassageiros = new ArrayList<>();
 
         try {
-            
+            File arquivoDeLeitura = new File(nomeDoArquivo);
         }
     }
 }
