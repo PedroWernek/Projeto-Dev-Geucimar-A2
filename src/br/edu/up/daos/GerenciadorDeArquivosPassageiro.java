4d895daad8656b089b4497c94d1ccbc83b00fc01
Ana Paula Endler <anapaulaendler@gmail.com>
2024-06-08 09:03:45 -0300

    adicionando o controle de arquivos de passageiros

---
+++ src/br/edu/up/daos/GerenciadorDeArquivosPassageiro.java
@@ -0,0 +1,19 @@
+package br.edu.up.daos;
+
+import java.util.*;
+import br.edu.up.modelos.Passageiro;
+
+public class GerenciadorDeArquivosPassageiro {
+    
+    public String header = "";
+    private String nomeDoArquivo = "";
+
+    /* lê os arquivos */
+    public List<Passageiro> getPassageiro() {
+        List<Passageiro> listaDePassageiros = new ArrayList<>();
+
+        try {
+            
+        }
+    }
+}
