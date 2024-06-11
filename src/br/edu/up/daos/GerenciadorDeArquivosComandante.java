e4859605338606fc870f02b202a9ef454393bc34
PedroWernek <pedrowcnetto@gmail.com>
2024-06-11 14:44:47 -0300

    arrumando o absolutePath do comandante

--- src/br/edu/up/daos/GerenciadorDeArquivosComandante.java
+++ src/br/edu/up/daos/GerenciadorDeArquivosComandante.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -16,8 +18,12 @@ import br.edu.up.util.Prompt;
 public class GerenciadorDeArquivosComandante {
 
   public String headerCom = "";
-  public String nomeArquivo = "C:/Users/user/Desktop/Projeto A2 - Geucimar/src/br/edu/up/docs/comandantes.csv";
-
+  public String nomeArquivo;
+  
+  public GerenciadorDeArquivosComandante(){
+    Path caminhoArquivo = Paths.get("src", "br", "edu", "up", "docs", "comandantes.csv");
+    nomeArquivo = caminhoArquivo.toAbsolutePath().toString();
+  }
   public List<Pessoa> getComandantes() {
     /**
      * Aqui estou pegando todos os Comandantes
